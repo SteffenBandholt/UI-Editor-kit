be3c6cfea707610f27c3948b8424f5636dff8d20
SteffenBandholt <info@bandholt.de>
2026-07-24 20:44:04 +0200

    Browser- und DOM-Laufzeit vollständig entfernen

--- src/selection/hoverOverlay.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const { createSelectionOverlayBase } = require("./overlayBase.js");
-function createHoverOverlay(options) { return createSelectionOverlayBase(Object.assign({ role: "hover", borderColor: "#2563eb", backgroundColor: "rgba(37, 99, 235, 0.10)", labelPrefix: "" }, options || {})); }
-module.exports = { createHoverOverlay };
